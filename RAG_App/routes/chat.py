60b09bf36c5583bd33ef829415ce6f522e8435de
Darsi Pushkar <pushkardarsi965@gmail.com>
2026-01-29 13:12:30 +0530

    modified chat file

--- RAG_App/routes/chat.py
+++ RAG_App/routes/chat.py
@@ -7,7 +7,7 @@ import uuid
 from ..ai.retrieval import search_similar_chunks
 from ..ai.llm_chat import chat_llm
 from ..chat_history import get_chat_history
-from ..utils.ambiguity import is_ambiguous
+#from ..utils.ambiguity import is_ambiguous
 
 
 
@@ -16,7 +16,7 @@ router = APIRouter(prefix="/chat",tags=["Chat"])
 @router.post("/")
 async def Chat(query: str, session_id: str | None = None, db: Session = Depends(get_db)):
 
-    chat_history=False
+    # chat_history=False
 
     if session_id is None:
         new_session =ChatSession()
@@ -24,14 +24,14 @@ async def Chat(query: str, session_id: str | None = None, db: Session = Depends(
         db.commit()
         db.refresh(new_session)
         session_id=str(new_session.id)
-    else:
-        chat_history=True
+    # else:
+    #     chat_history=True
     
-    if is_ambiguous(query, chat_history):
-        return {
-            "session_id": session_id,
-            "answer": "Could you please clarify what you are referring to?"
-        }
+    # if is_ambiguous(query, chat_history):
+    #     return {
+    #         "session_id": session_id,
+    #         "answer": "Could you please clarify what you are referring to?"
+    #     }
     
     user_message=ChatMessage(session_id=UUID(session_id),role="user",content=query)
     db.add(user_message)
